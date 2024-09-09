74f223eab02d747db8e44a9bb527c4ea5b272233
muhammed <ajferaju9961@gmail.com>
2024-09-09 12:01:57 +0530

    configured cors in user-service

--- backend/User Service/src/entities/user.entity.ts
+++ backend/User Service/src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, Types } from "mongoose";
 import bcrypt from "bcrypt";
 import { ObjectId } from "mongodb";
 
--- backend/User Service/src/index.ts
+++ backend/User Service/src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
+import cors from "cors";
 import userRoute from "./routes/user.routes";
 import { connectDB } from "./frameworks/mongo/connect";
 
 const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
