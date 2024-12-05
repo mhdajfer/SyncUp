2efba4b312f044da83e04f775f37c5025ee1abf4
muhammed <ajferaju9961@gmail.com>
2024-12-05 10:11:48 +0530

    fix: bug in log time display

--- frontend/src/Components/Log-time/LogTime.tsx
+++ frontend/src/Components/Log-time/LogTime.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Button } from "@/Components/ui/button";
 import { Badge } from "@/Components/ui/badge";
 import { Task } from "@/interfaces/Project";
 import { differenceInSeconds } from "date-fns";
 import { editTask } from "@/api/projectService/project";
 import { toast } from "sonner";
-import { LogTimeDisplay } from "@/lib/date-format";
 
 export default function LogTime({
   setTask,
@@ -17,6 +16,7 @@ export default function LogTime({
   task: Task;
 }) {
   const [isLogging, setIsLogging] = useState(false);
+  const [totalLogTime, setTotalLogTime] = useState("");
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const updateTaskOnServer = async (updatedTask: Task) => {
@@ -87,12 +87,32 @@ export default function LogTime({
     // Make the API call to update the task on the server
     updateTaskOnServer(updatedTask);
 
+    const formattedLogTime = formatToHHMMSS(updatedTask.log_time.total_time);
+
+    setTotalLogTime(formattedLogTime);
+
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
     }
     console.log("Logging stopped");
   };
+
+  const formatToHHMMSS = (seconds: number): string => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+
+    return [hours, minutes, secs]
+      .map((val) => String(val).padStart(2, "0")) // Ensure 2-digit formatting
+      .join(":");
+  };
+
+  useEffect(() => {
+    const formattedLogTime = formatToHHMMSS(task.log_time.total_time);
+
+    setTotalLogTime(formattedLogTime);
+  }, [task.log_time.total_time]);
 
   return (
     <div className=" my-4 w-full max-w-md mx-auto p-4 space-y-4 flex flex-col items-center  ">
@@ -111,16 +131,20 @@ export default function LogTime({
         >
           Stop Log
         </Button>
-        <Badge variant={isLogging ? "default" : "secondary"}>
-          {isLogging ? "started" : "Not started"}
+        <Badge variant={task.log_time.start_time ? "default" : "secondary"}>
+          {task.log_time.start_time ? "started" : "Not started"}
         </Badge>
       </div>
       <div>
         <h3>
-          <span className="text-gray-400 font-extralight text-sm">
-            {LogTimeDisplay(task.log_time.total_time)}
+          <span className="text-neutral-300">
+            {task.log_time.total_time
+              ? ` Total Time Logged : `
+              : ` Not started working yet!`}
           </span>
-          {task.log_time.total_time ? ` logged` : ` Not started working yet!`}
+          <span className="text-gray-400 font-extralight text-sm">
+            {totalLogTime}
+          </span>
         </h3>
       </div>
     </div>
