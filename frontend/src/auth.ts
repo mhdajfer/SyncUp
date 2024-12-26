0ce122060bef1f6bca2f8c8f949a861106a5366a
muhammed <ajferaju9961@gmail.com>
2024-12-26 13:19:24 +0530

    feat: config ci/cd for client

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -11,7 +11,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     async jwt({ token, user }) {
-      // If the user is signing in for the first time, add their ID to the token
+      // If the user is signing in for the first time, add their ID to the token...
       if (user) {
         token.sub = user.id;
       }
