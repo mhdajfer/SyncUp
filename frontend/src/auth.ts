95f413306ef7962ea67a4d22efc10862e958bc27
muhammed <ajferaju9961@gmail.com>
2025-01-02 11:07:29 +0530

    fix: resolve nextAuth(google)

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,21 +1,6 @@
 import NextAuth from "next-auth";
-
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      // If the user is signing in for the first time, add their ID to the token...
-      if (user) {
-        token.sub = user.id;
-      }
-      return token;
-    },
-  },
+  providers: [Google],
 });
