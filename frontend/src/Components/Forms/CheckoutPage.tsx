6174e1adcab2c54db8d054d0c73fc5ee43a8de70
muhammed <ajferaju9961@gmail.com>
2025-01-10 13:35:14 +0530

    fix: stripe deploy

--- frontend/src/Components/Forms/CheckoutPage.tsx
+++ frontend/src/Components/Forms/CheckoutPage.tsx
@@ -4,11 +4,13 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CheckoutForm } from "@/Components/Forms/CheckoutForm";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
-
 export default function CheckoutPage() {
+  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+    return console.log("stripe key not found");
+
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold text-center mb-6">Checkout</h1>
