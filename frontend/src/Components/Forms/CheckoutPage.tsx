018e96c4439e3c6a8c51650beeb3672b356dd2f8
muhammed <ajferaju9961@gmail.com>
2025-01-10 13:42:57 +0530

    revert: stripe config

--- frontend/src/Components/Forms/CheckoutPage.tsx
+++ frontend/src/Components/Forms/CheckoutPage.tsx
@@ -4,13 +4,11 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CheckoutForm } from "@/Components/Forms/CheckoutForm";
 
+const stripePromise = loadStripe(
+  "pk_test_51QOxa7JZQtcfGAZqWF7l0Bm4rh7TgbZhXX3bS5s2DzpadDiDP1t7wyCH8zweL79NsN0GdYmYka60Knp04AaGnjR7008PsDGqfg"
+);
 export default function CheckoutPage() {
-  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
-    return console.log("stripe key not found");
 
-  const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-  );
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold text-center mb-6">Checkout</h1>
