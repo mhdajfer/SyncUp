6c59af17f35e8c29d3c42f3f7e495091d1ecca2c
muhammed <ajferaju9961@gmail.com>
2025-04-22 16:39:44 +0530

    Revert "fix: s3 put req"
    This reverts commit 265c46012ac51fa595d46e9ffb6ba8949e89bb32.

--- frontend/src/lib/S3.ts
+++ frontend/src/lib/S3.ts
@@ -26,7 +26,7 @@ export const uploadFileToS3 = async (
   try {
     const options = {
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/pdf",
       },
     };
     console.log("Uploading file:", file.name, "Size:", file.size);
