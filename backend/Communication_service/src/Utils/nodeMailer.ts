3e1d1b9046596b1f6d26884592b8201af2599200
muhammed <ajferaju9961@gmail.com>
2025-04-25 11:56:44 +0530

    test: nodemailer cred

--- backend/Communication_service/src/Utils/nodeMailer.ts
+++ backend/Communication_service/src/Utils/nodeMailer.ts
@@ -17,13 +17,13 @@ export const sendMail = (
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.NODEMAILER_EMAIL,
-      pass: process.env.NODEMAILER_PASSWORD,
+      user: "ajferaju9961@gmail.com",
+      pass: "zgyz kqlo dkuz jbhz",
     },
   });
 
   const mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
+    from: "ajferaju9961@gmail.com",
     to: email,
     subject: `New Task Assigned: ${taskName}`,
     html: `
